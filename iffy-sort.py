92e1ff5e1d814af7825fedbe9a838fb5251e465e
BananaGrill <49271965+bea19962@users.noreply.github.com>
2023-11-29 16:38:10 +0100

    negative number sort

--- iffy-sort.py
+++ iffy-sort.py
@@ -1,25 +1,34 @@
 #finds the biggest number
 def largest(arr):
     max_val = arr[0]
-    for i in range(1, len(arr)):
+    for i in range(len(arr)):
         if arr[i] > max_val:
             max_val = arr[i]
     return max_val
+    
+#finds the smallest number
+def smallest(arr):
+    min_val = arr[0]
+    for i in range(len(arr)):
+        if arr[i] < min_val:
+            min_val = arr[i]
+    return min_val
 
 def silly_sort(arr):
     if not arr:
         return []
     else:
         max_val = largest(arr)
+        min_val = smallest(arr)
         sorted_array = []
         #loops the biggest number of times and checks if the number exists in the array
-        for i in range(1, max_val + 1):
+        for i in range(min_val, max_val + 1):
             if i in arr:
                 sorted_array.append(i)
         return sorted_array
 
 # Define the array
-arr = [1, 4, 8, 3,12,2,6,22,13]
+arr = [-1, 4, -8, -3]
 
 # Print the result
 print(silly_sort(arr))
